97601cebf64fc854842d549a1b6e6f40c1cde984
dokawoody <dokawoody@gmail.com>
2021-08-30 14:31:53 +0900

    edited.

--- sample.js
+++ sample.js
@@ -8,6 +8,6 @@ $("#btn1").on("click", function()  {
   const article = document.getElementById('electric-cars');
   // jQueryを使って画面にメッセージを表示する
   $("#tBox").val(message);
-  $("#tBox").val(article.dataset.columns);
-  $("#tBox").val(article.dataset.indexNumber);
+  $("#tBox1").val(article.dataset.columns);
+  $("#tBox2").val(article.dataset.indexNumber);
 });
