cdc6374724311a2df4bb4f194da15663cdb46a5f
dokawoody <dokawoody@gmail.com>
2021-08-30 14:29:48 +0900

    Edit

--- sample.js
+++ sample.js
@@ -5,6 +5,9 @@ $("#btn1").on("click", function()  {
   let month = date.getMonth() + 1;
   let day = date.getDate();
   let message = `今日は${year}年${month}月${day}日です`
+  const article = document.getElementById('electric-cars');
   // jQueryを使って画面にメッセージを表示する
   $("#tBox").val(message);
+  $("#tBox").val(article.dataset.columns);
+  $("#tBox").val(article.dataset.indexNumber);
 });
